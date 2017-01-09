4ff539783c5f7453c24dd272904623e5f0aa10a9
Dayvid Emerson <dayvidemerson96@gmail.com>
2017-01-08 23:30:56 -0300

    Redirecionando estudante para tela de refeições

--- core/views.py
+++ core/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from student.models import Student
 
 # Página inicial
 
 
 def index(request):
+    if ('user' in request and
+            Student.objects.filter(user_ptr=request.user).exists()):
+        return HttpResponseRedirect('/meal/student/list/')
+
     return render(request, 'index.html')
